3ec79eee110f7c4608390423e2cdfadf70bcb98d
Grégoire Pinte <gregoire.pinte@gmail.com>
2025-05-31 06:58:06 -0500

    feat: create public layout

---
+++ app/layouts/public.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fixed inset-0 flex flex-col">
+    <LayoutHeaderPublic />
+    <div class="flex flex-col flex-1 overflow-y-auto">
+      <main class="flex flex-col flex-1 gap-4 px-3 pt-4 pb-10 md:overflow-y-auto">
+        <slot />
+      </main>
+      <LayoutFooterPublic />
+    </div>
+  </div>
+</template>
