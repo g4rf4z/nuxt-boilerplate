5b4fcbf3162f9b5640a3605e1f949c1542b580f1
Grégoire Pinte <gregoire.pinte@gmail.com>
2025-06-10 21:33:14 +0200

    feat: create layout components

---
+++ app/components/LayoutHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="h-16 border-b border-gray-200 dark:border-gray-800 flex flex-row gap-4 items-center justify-between px-4">
+    <h1 class="title flex flex-row gap-2 items-center">
+      Epsi Edge ®
+    </h1>
+
+    <UIToggleTheme />
+  </header>
+</template>
